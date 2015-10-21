9fb768f0943ec4ecac850ae59e6600a9097212ca
Edward Zhu <edward.r.zhu@gmail.com>
2015-10-21 12:49:05 -0600

    added line parsing for client, makes all exe now

--- PA2/dfc.c
+++ PA2/dfc.c
@@ -24,17 +24,20 @@
 // #include <sys/stat.h>
 
 int BUFFER_SIZE = 1024;
+#define MAXSIZE  8192
 
 struct Config {
-    char    dps1_port[20];
-    char    dps2_port[20];
-    char    dps3_port[20];
-    char    dps4_port[20];
+    char    dfs1_port[20];
+    char    dfs2_port[20];
+    char    dfs3_port[20];
+    char    dfs4_port[20];
     char    Username[50];
     char    Password[50];
 } config_dfc;
 
 void parse_config(const char *);
+
+
 
 void parse_config(const char *filename) {
     char *line;
@@ -52,13 +55,13 @@ void parse_config(const char *filename) {
         // and set it in the config_dfc object
         if (!strcmp(head, "Server")) {
             if (!strcmp(second, "DFS1"))
-                strcat(config_dfc.dps1_port, host_port);
+                strcat(config_dfc.dfs1_port, host_port);
             if (!strcmp(second, "DFS2"))
-                strcat(config_dfc.dps2_port, host_port);
+                strcat(config_dfc.dfs2_port, host_port);
             if (!strcmp(second, "DFS3"))
-                strcat(config_dfc.dps3_port, host_port);
+                strcat(config_dfc.dfs3_port, host_port);
             if (!strcmp(second, "DFS4"))
-                strcat(config_dfc.dps4_port, host_port);
+                strcat(config_dfc.dfs4_port, host_port);
         } 
 
         // Parce/Set Username
@@ -71,14 +74,36 @@ void parse_config(const char *filename) {
             strcat(config_dfc.Password, second);
         } 
     }
-    
+
+    // Debugging
+    printf("dfsport1: %s\n", config_dfc.dfs1_port);
+    printf("dfsport2: %s\n", config_dfc.dfs2_port);
+    printf("dfsport3: %s\n", config_dfc.dfs3_port);
+    printf("dfsport4: %s\n", config_dfc.dfs4_port);
+    printf("Username: %s\n", config_dfc.Username);
+    printf("Password: %s\n", config_dfc.Password);
+
+
     fclose(conf_file);
 }
 
-int main() {
+int main(int argc, char **argv) {
+    char *conf_file, buf[MAXSIZE];
+    int n;
+
+    // If no parameter given, send error
+    // If it is given, then set conf_file to argv[1]
+    if (argc != 2) {
+        fprintf(stderr, "Config file required.");
+        exit(0);
+    }
+    conf_file = argv[1];
+
     // Parse through wsconf file and set variables when needed.
-    parse_config("dfc.conf");
+    parse_config(conf_file);
     // End parse
+
+
 
     return EXIT_SUCCESS;
 }
