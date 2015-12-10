f444f81671f8eeef09b1857c14520f0acb84b220
Edward Zhu <edward.r.zhu@gmail.com>
2015-12-09 18:07:23 -0700

    added logging

--- t_proxy/proxy.c
+++ t_proxy/proxy.c
@@ -1,7 +1,7 @@
 /*
     Edward Zhu
     Programming Assignment 3
-    Proxy Server - in C
+    Transparent Proxy Server - in C
 
     proxy.c
     Setting up a Proxy Server
@@ -34,75 +34,105 @@
 #include <stdio.h>
 
 int BUFFER_SIZE = 2048;
+// For mac to make sol_ip working
+// #ifndef SOL_IP
+// #define SOL_IP IPPROTO_IP
+// #endif
 
-/*
-    Checked if HTTP version is 1.0, then parces IP address
-
-    Reference:
-        http://www.beej.us/guide/bgnet/output/html/singlepage/bgnet.html#getaddrinfo
-*/
-// void get_request(int sock, char *url, char *http_version, char *request){
-//     // int status;
-//     struct hostent *server;
-//     struct sockaddr_in serveraddr;
-//     char response[2048];
-
-//     if(strncmp(http_version, "HTTP/1.0", 8) == 0){
-//         printf("HTTP Version is correct!\n");
-
-//         server = gethostbyname(url);
-
-//         if (server == NULL){
-//             printf("gethostbyname() failed\n");
-//             printf("URL given is probabaly garbage.\n");
-//         }
-
-//     printf("Official name is: %s\n", server->h_name);
-//     printf("IP address: %s\n", inet_ntoa(*(struct in_addr*)server->h_addr));
-
-
-//         // connect to the server
-//         bzero((char *) &serveraddr, server->h_length);
-
-//         serveraddr.sin_family = AF_INET;
-//         // server.sin_addr.s_addr = inet_addr(host);
-//         bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length );
-//         serveraddr.sin_port = htons(80);
-
-//         int tcpSocket = socket(AF_INET, SOCK_STREAM, 0);
-
-//         if (connect(tcpSocket, (struct sockaddr *) &serveraddr, sizeof serveraddr ) < 0){
-//             printf("\nError Connecting");
-//         }
-
-//         strcat(request, "\r\n");
-//         // Send request to socket
-//         if (send(tcpSocket, request, strlen(request), 0) < 0){
-//             printf("Error with send()");
-//         }
-//         bzero(response, 1000);
-//         recv(tcpSocket, response, 999, 0);
-//         write(sock, response, strlen(response));
-
-//     } else {
-//         printf("HTTP Version is NOT correct!\n");
-//     }
-// }
 
 /*
     Process the request that a client requests
     Example:
         GET http://www.google.com HTTP/1.0
 */
-void process_request(int socket){
+void process_request(int sock){
     char buf[BUFFER_SIZE];
-    // char *token;
+    int server_sock;
     int read_size = 0;
     int len = 0;
     // char command[64], full_url[256], http_version[64], url[128], service[64];
 
+    struct sockaddr_in s_hints;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in p_addr;
+    struct sockaddr_in d_addr;
+    socklen_t s_addrlen; 
+    socklen_t p_addrlen; 
+    socklen_t d_addrlen; 
+    s_addrlen = sizeof(struct sockaddr_in);
+    p_addrlen = sizeof(struct sockaddr_in);         
+    d_addrlen = sizeof(struct sockaddr_in);
+
+    server_sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (server_sock == -1)
+    {
+        printf("Could not create socket\n");
+    }
+
+    s_hints.sin_family = AF_INET;
+    s_hints.sin_port = 0;
+    s_hints.sin_addr.s_addr = INADDR_ANY; 
+    bzero(&s_hints.sin_zero, 8);
+
+    if((bind(server_sock, (struct sockaddr *) &s_hints, sizeof(s_hints))) < 0){
+        perror("bind failed. Error");
+        exit(-1);
+    }
+    //Need information for SNAT rules, getting the server info
+    if((getsockname(server_sock, (struct sockaddr *) &server_addr, &s_addrlen)) < 0){
+        perror("getsockname failed. Error");
+        exit(-1);       
+    }
+    //Need information for SNAT rules, getting the client info
+    if((getpeername(sock, (struct sockaddr *) &p_addr, &p_addrlen)) < 0){
+        perror("getpeername: ");
+        exit(-1);       
+    }
+    //Need information for SNAT rules, getting the original destination (in write up)
+    if((getsockopt(sock, SOL_IP, 80, (struct sockaddr *) &d_addr, &d_addrlen)) < 0){
+        perror("getsockopt: ");
+        exit(-1);
+    }
+
+
+    char SnatRules[BUFFER_SIZE];
+    sprintf(SnatRules, "iptables -t nat -A POSTROUTING -p tcp -j SNAT --sport %d --to-source %s", ntohs(server_addr.sin_port), inet_ntoa(p_addr.sin_addr));
+    printf("%s\n", SnatRules);  
+    system(SnatRules);
+
+    int server_fd;
+    //Finally, connect to the server
+    server_fd = connect(server_sock, (struct sockaddr *) &d_addr, d_addrlen);
+    if (server_fd == -1){
+        exit(1);
+    }
+
+    char *logfile;
+    char logs[BUFFER_SIZE];
+    time_t currentTime;
+    currentTime = time(NULL);
+    //Getting the date and time
+    logfile = ctime(&currentTime);
+    //Replacing the new line character
+    logfile[strlen(logfile)-1] = ' ';
+    //Add the client's IP address to the logfile
+    strcat(logfile, inet_ntoa(p_addr.sin_addr));
+    //Convert the client's port number to a string
+    sprintf(logs, " %d ", ntohs(p_addr.sin_port));
+    strcat(logfile, logs);
+    //Add the server's IP address to the logfile
+    strcat(logfile, inet_ntoa(d_addr.sin_addr));
+    memset(logs, 0, BUFFER_SIZE);
+    //Convert the server's port number to a string
+    sprintf(logs, " %d ", ntohs(d_addr.sin_port));
+    strcat(logfile, logs);
+
+    
+
+
+
     // Keep reading in what client sends
-    while ((read_size = recv(socket, &buf[len], (BUFFER_SIZE-len), 0)) > 0)
+    while ((read_size = recv(sock, &buf[len], (BUFFER_SIZE-len), 0)) > 0)
     { 
         char line[read_size];
         strncpy(line, &buf[len], sizeof(line));
@@ -113,15 +143,29 @@ void process_request(int socket){
 
     } 
 
+
+    FILE * FileLog = fopen("logs.txt", "a+");
+    fprintf(FileLog, "%s", logfile);
+    fclose(FileLog);
+
+
     if(read_size == 0) {
         puts("Client disconnected");
         fflush(stdout);
     } else if(read_size == -1){
         perror("recv failed");
     }
+
     return;
 }
 
+void *find_which_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET)
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
 /*
     Opens a sockets and listens on that port.
@@ -129,8 +173,9 @@ void process_request(int socket){
 */
 int open_port(){
     int sockfd , client_sock, c, *new_sock;
-    struct sockaddr_in server , client;
+    struct sockaddr_in client;
     char DnatRules[BUFFER_SIZE];
+    char s[INET6_ADDRSTRLEN];
 
     // struct sockaddr_storage their_addr;
     char *port = NULL;
@@ -181,6 +226,11 @@ int open_port(){
     while( (client_sock = accept(sockfd, (struct sockaddr *)&client, (socklen_t*)&c)) )
     {
         printf("Connection accepted\n");
+
+        // inet_ntop(client.ss_family, find_which_addr((struct sockaddr *)&client), s, sizeof s);
+
+        printf("server port %s: got connection from %s\n", port, s);
+
         new_sock = malloc(1);
         *new_sock = client_sock;
          
@@ -198,6 +248,7 @@ int open_port(){
     }
     return 0;
 }
+
 
 /*
     Main function
